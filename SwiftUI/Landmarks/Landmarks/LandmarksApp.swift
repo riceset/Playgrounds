3d9979234c53b1af938502393c6e7e7be2723eb6
komeno <tkomeno@student.42tokyo.jp>
2024-01-16 08:36:52 +0900

    Test commit from Xcode

---
+++ SwiftUI/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Komeno on 2024/01/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
